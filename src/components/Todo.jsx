a1086bb943c4ed46c24e701c9fe2473cb22b4430
chusamaikram <usamachaudhary0345@gmail.com>
2025-11-25 16:32:37 +0500

    added local storage

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,16 @@
-import { li } from "framer-motion/client";
-import { useState } from "react";
+
+import { useState, useEffect, use } from "react";
 
 export default function TodoApp() {
 
     const [newtodo, setNewtodo] = useState("");
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(() => {
+        const savedData = localStorage.getItem("todos")
+        return savedData ? JSON.parse(savedData) : []
+    });
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,6 +27,7 @@ export default function TodoApp() {
         setTodos(updatedTodos);
 
     }
+
     return (
         <>
             <div className="max-w-[1400px] px-20 mx-auto">
@@ -36,9 +43,9 @@ export default function TodoApp() {
                     <ul className="flex flex-col items-start gap-2 mt-4 border border-gray-300 rounded-md p-4 w-1/2 ">
                         {todos.map((todo, index) => (
                             <li key={index} className="flex items-center justify-between gap-4 py-2 w-full">
-                                <span className={`${todo.completed ? "line-through" : "no-underline"}`}> {index+1}:- I have to {todo.text}</span>
+                                <span className={`${todo.completed ? "line-through" : "no-underline"}`}> {index + 1}:- I have to {todo.text}</span>
                                 <button className={`py-2 px-3 font-semibold  ${todo.completed ? " bg-green-400 cursor-not-allowed" : "bg-red-400 cursor-pointer"} text-white rounded-sm`}
-                                   onClick={() => handleDelete(index)}>{todo.completed ? "Completed" : "Delete"}</button>
+                                    onClick={() => handleDelete(index)}>{todo.completed ? "Completed" : "Delete"}</button>
 
                             </li>
                         ))}
